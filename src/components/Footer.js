0d8851b0194896ef6faf5c2840d0573642f39e17
* <frabby983@gmail.com>
2022-08-04 09:04:19 +0600

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import styles from './css/footer.module.css'
+import {Link} from 'react-router-dom'
 
 export default function Footer(){
   return(
     <>
       <footer class={styles.footer}>
-        <h4>Created by <a class="" href="#">fazle rabbi</a></h4>
+        <h4>Created by <Link class="" to="https://cutt.ly/rabbi">fazle rabbi</Link></h4>
         <p>&copy;2022 Phone-Customization</p>
       </footer>    
     </>
